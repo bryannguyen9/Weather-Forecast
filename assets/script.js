e65a521e0cc9c0005839a15b041a25fc3e74d7ea
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-08 16:27:10 -0700

    added current city info when user searches a city, still need to implement dayJS so that user can get CURRENT data not average and need to implement 5 day forecast section

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ var ApiKey = "0851b496d12ca4c702ac618ee6340d10";
 var cityInput = document.getElementById("city");
 var currentCity = document.querySelector('#current-city-info');
 
+var cityInfoContainer = document.getElementById('current-city-info-2');
+
 function getApi() {
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=" + ApiKey;
 
@@ -19,13 +21,27 @@ function getApi() {
         }).then(function(data){
             console.log(data);
             console.log("Name ", data.name);
+            //current city info name, temp, wind, humidity
             currentCity.textContent = "City: " + data.name;
+            var currTemp = document.createElement('p');
+            currTemp.textContent = "Temperature(F): " + (((((data.main.temp)-273.15)*9)/5)+32);
+            cityInfoContainer.append(currTemp);
+
+            var currWind = document.createElement('p');
+            currWind.textContent = "Wind Speeds(MPH): " + (((data.wind.speed)/1609.34)*3600);
+            cityInfoContainer.append(currWind);
+
+            var currHumidity = document.createElement('p');
+            currHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+            cityInfoContainer.append(currHumidity);
+
         }).catch(function(error) {
             console.log(error);
             currentCity.textContent = "Error fetching weather data";
         });
 }
   
+//event listener on click of form submit to first DOMLoad then run the function of getAPI()
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("submit-button").addEventListener("click", function(event) {
         event.preventDefault(); // prevent form submission
@@ -33,4 +49,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
  
-
+  //need to add dayJS for current date and then add 5 day forecast implemented within current city
+  
