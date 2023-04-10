0c0177e1bd1a63841db4b5b50792035f8736bbf6
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-09 20:01:41 -0700

    added forecast data for 1st day and it repeats on the same card, need to add anothe for loop index or different id's to different day containers for forecast info to append to the next card not all in one card

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,18 @@ var currentCityHeader = document.getElementById('current-city-info');
 
 var cityInfoContainer = document.getElementById('current-city-info-2');
 
+var forecastData = document.getElementById('forecast-data')
+
 //api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
 
 function getApi() {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&units=imperial&appid=" + ApiKey;
+    var currentWeatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&units=imperial&cnt=5&appid=" + ApiKey;
+    var fiveDayForecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&units=imperial&cnt=5&appid=" + ApiKey;
 
-    fetch(queryURL)
+    fetch(currentWeatherQueryURL)
         .then(function (response) {
             console.log(response);
-            // We check whether the response.status equals 200, as follows:
             if (response.status === 200) {
-                //If it does, we assign the status code from response.status to the textContent
                 return response.json();
             }
             else {
@@ -23,13 +24,12 @@ function getApi() {
         }).then(function(data){
             console.log(data);
             console.log("Name ", data.name);
-            //current city info name, temp, wind, humidity
             var currName = document.createElement('p');
             currName.textContent = "City: " + data.name;
             currentCityHeader.append(currName);
-            var today = dayjs(today).format('dddd MMM DD/YY');
+            //added dayjs for date
+            var today = dayjs(today).format('dddd MMM DD, YYYY');
             currentCityHeader.append(today);
-           
 
             var currTemp = document.createElement('p');
             currTemp.textContent = "Temperature(F): " + data.main.temp;
@@ -43,23 +43,71 @@ function getApi() {
             currHumidity.textContent = "Humidity: " + data.main.humidity + "%";
             cityInfoContainer.append(currHumidity);
 
-            //add 5 day forecast info
-            //use dayjs.add to add a day to each date prior
+    // make another API request for the 5-day forecast data
+    fetch(fiveDayForecastQueryURL)
+        .then(function (response) {
+            if (response.status === 200) {
+            return response.json();
+            } else {
+            throw new Error('Error fetching weather data');
+            }
+        }).then(function (data) {
+            try {
+            // check if data and data.list are defined
+                if (data && data.list) {
+                    console.log(data);
+                    // rest of your code for displaying current weather
 
-        }).catch(function(error) {
+                    // loop through 5 day forecast data
+                    for (var i = 0; i < data.list.length; i++) {
+                    // display forecast data for each day
+
+                    //need a way to identify which exact forecastData container to append dayData to at end need another index for day containers.
+                        var dayData = document.createElement('div');
+                        dayData.classList.add('day-data');
+
+                        var date = dayjs(data.list[i].dt_txt).format('dddd MMM DD, YYYY');
+                        var dateEl = document.createElement('p');
+                        dateEl.textContent = date;
+                        dayData.append(dateEl);
+
+                        var temp = document.createElement('p');
+                        temp.textContent = "Temperature(F): " + data.list[i].main.temp;
+                        dayData.append(temp);
+
+                        var wind = document.createElement('p');
+                        wind.textContent = "Wind Speeds(MPH): " + (((data.list[i].wind.speed)/1609.34)*3600);
+                        dayData.append(wind);
+
+                        var humidity = document.createElement('p');
+                        humidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+                        dayData.append(humidity);
+
+                        forecastData.append(dayData);
+                    }
+                } else {
+                    throw new Error('Unexpected response format');
+                }
+            } catch (err) {
+                console.error(err);
+                currName.textContent = "Error fetching weather data";
+                currentCityHeader.append(currName);
+                }
+        }).catch(function (error) {
             console.log(error);
             currName.textContent = "Error fetching weather data";
             currentCityHeader.append(currName);
         });
+        })
 }
-  
+
 //event listener on click of form submit to first DOMLoad then run the function of getAPI()
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("submit-button").addEventListener("click", function(event) {
         event.preventDefault(); // prevent form submission
         getApi();
     });
-  });
+});
  
-  //need to add dayJS for current date and then add 5 day forecast implemented within current city
+ 
  
